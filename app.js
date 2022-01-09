300b9121e421c49a14b4dd7aad87a076f9e35edf
Genesis Barrios <genesisbarriosdev@gmail.com>
2022-01-09 14:14:33 -0500

    add host var

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ app.get("/", function(req,res){
     res.render('index');
 });
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT,function(){
-    console.log("listening");
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
 });
